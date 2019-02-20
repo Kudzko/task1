7aa6ce25a7f0dac64e4aca3a4a5c8c44c2556637
AKudko <powering@mail.ru>
2019-02-20 14:23:13 +0300

    first commit

---
+++ Task1/src/main/java/Test.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        Object[] test = new Object[5];
+//        for (int i=0; i< test.length; i++){
+//            System.out.println(test[i]);
+//        }
+//
+//        test = new Object[2*test.length];
+//        System.out.println(test.length);
+
+        List <Object> list = new ArrayList<Object>();
+        list.add(new Object());
+        System.out.println( list.get(1));
+       // list.get(1);
+    }
+}
--- Task1/src/main/java/by/epam/javawebtraining/kudko/task01/model/logic/Director.java
+++ Task1/src/main/java/by/epam/javawebtraining/kudko/task01/model/logic/Director.java
@@ -1,5 +1,5 @@
 package by.epam.javawebtraining.kudko.task01.model.logic;
 
 public class Director {
-    
+
 }
